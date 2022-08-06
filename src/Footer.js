c612f6b159c99395802a0317fd032d8dc651e7c7
Luis Adrián Chávez Fregoso <biolacf@gmail.com>
2022-08-06 17:35:36 -0500

    Changes media query for header and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="App-footer max-w-5xl w-full py-4 px-4 lg:px-8 border-t border-slate-900/10">
-    <h3 className="text-sm lg:text-center">Información obtenida en Banxico de la serie SP1 actualizada al mes de junio de 2022.</h3>
+    <h3 className="text-stone-600 text-sm sm:text-center">Información obtenida en Banxico de la serie SP1 actualizada al mes de junio de 2022.</h3>
   </footer>
 );
 
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 const Header = () => (
   <header className="App-header max-w-5xl w-full py-4 px-4 lg:px-8 border-b border-slate-900/10 font-bold">
-    <h1 className="text-2xl lg:text-center uppercase">💵 Salario justo</h1>
+    <h1 className="text-2xl sm:text-center uppercase">💵 Salario justo</h1>
   </header>
 );
 
